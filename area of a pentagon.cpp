4841c811686ec1bf382330045777a1ab402612d2
EC2 Default User <ec2-user@ip-172-31-13-154.ec2.internal>
2022-04-03 19:48:31 +0000

    more and more

---
+++ area of a pentagon.cpp
@@ -0,0 +1,39 @@
+// Created by: Paterry Baptichon Junior
+// Created on: 2022-04-03
+// This program calculates the area of a pentagon
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    // Declaring variables
+    int base;
+    int height;
+    float area;
+  
+
+    // Asking user for the input of the base and height of the pentagon
+    std::string userAnswer;
+    std::cout << "Finding surface area of a pentagon. " << std::endl;
+    std::cout << "Enter the base of the pentagon (cm): ";
+    std::cin >> base;
+    std::cout << "Enter the height of the pentagon (cm): ";
+    std::cin >> height;
+
+    // The formula and calculation
+    area = (0.5)*base*height;
+    // display the user the area of a pentagon\
+    
+    std::cout << "" << std::endl;
+    std::cout << "The area is " << (area) << "cm^2" << std::endl;
+     // Ask if user would like to calculate again
+  std::cout << "Would you like to calculate again? Y or N\n";
+  std::cin >> userAnswer;
+    if (userAnswer == "N" || "n") {
+      return 0;
+    }
+  if (userAnswer == "Y" || "y") {
+      main();
+      
+  }
+}
